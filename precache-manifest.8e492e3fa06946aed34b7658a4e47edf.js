c3c395f5f8d60868c8ad868cd6ed4e6e7e998be6
daliabdmalek <49783051+daliabdmalek@users.noreply.github.com>
2019-05-10 12:24:10 +0200

    Updates

---
+++ precache-manifest.8e492e3fa06946aed34b7658a4e47edf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94067fe343202e5471be3b3317ba0802",
+    "url": "/React-Docplanner/index.html"
+  },
+  {
+    "revision": "d991f9091a8c6715da23",
+    "url": "/React-Docplanner/static/css/main.3cbef294.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/React-Docplanner/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d991f9091a8c6715da23",
+    "url": "/React-Docplanner/static/js/main.d00c3343.chunk.js"
+  },
+  {
+    "revision": "32fcb11e93ec908e0869",
+    "url": "/React-Docplanner/static/js/runtime~main.1d33997c.js"
+  }
+]);
